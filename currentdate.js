ee457075bd34cd2b3b0fcb2603ec840f6b456b42
Ken Strobell <35313618+str17024@users.noreply.github.com>
2018-02-16 13:35:41 -0700

    Update currentdate.js

--- currentdate.js
+++ currentdate.js
@@ -1,7 +1,18 @@
 var today = new Date();
-var dd = today.getDay();
+var dd = today.getDay()
 var mm = today.getMonth() + 1; //January is 0!
 var yyyy = today.getFullYear();
+
+var weekday = new Array(7);
+    weekday[0] = "Sunday";
+    weekday[1] = "Monday";
+    weekday[2] = "Tuesday";
+    weekday[3] = "Wednesday";
+    weekday[4] = "Thursday";
+    weekday[5] = "Friday";
+    weekday[6] = "Saturday";
+	
+var n = weekday[dd];
 
 if (dd < 10) {
     dd = '0' + dd
@@ -11,5 +22,5 @@ if(mm < 10) {
     mm = '0' + mm
 } 
 
-today = dd + '/' + mm + '/' + yyyy;
+today = n + '/' + mm + '/' + yyyy;
 document.write(today);
