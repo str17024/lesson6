e7c02fb33158c9d7e285032bf2151c550038172e
Ken Strobell <35313618+str17024@users.noreply.github.com>
2018-02-18 00:28:30 -0700

    Update hamburger.js

--- hamburger.js
+++ hamburger.js
@@ -1,5 +1,5 @@
 function toggleMenu(){
-	console.log(document.getElementById("primaryNav").classlist;
+	//console.log(document.getElementById("primaryNav").classlist;
 	document.getElementById("primaryNav").classList.toggle("hide");
 	
 }
